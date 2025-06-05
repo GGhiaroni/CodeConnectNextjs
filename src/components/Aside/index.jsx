b78f0136e62348dca000c785804d33a3230f2a2f
GGhiaroni <gghiaronitiziano@gmail.com>
2025-06-05 13:51:07 -0300

    comecando a criar a estrutura do card post

--- src/components/Aside/index.jsx
+++ src/components/Aside/index.jsx
@@ -1,7 +1,10 @@
+import Image from "next/image";
+import logo from "./logo.png";
+
 export const Aside = () => {
   return (
     <aside className="rounded-lg px-4 py-10 bg-[#171D1F]">
-      <img src="/logo.png" alt="Logo Code Connect" />
+      <Image src={logo} alt="Logo Code Connect" />
     </aside>
   );
 };
---
+++ src/components/CardPost/index.jsx
@@ -0,0 +1,17 @@
+import Image from "next/image";
+
+export const CardPost = ({ post }) => {
+  return (
+    <article>
+      <header>
+        <figure>
+          <Image />
+        </figure>
+      </header>
+      <section>titulo texto</section>
+      <footer>
+        <Avatar />
+      </footer>
+    </article>
+  );
+};
