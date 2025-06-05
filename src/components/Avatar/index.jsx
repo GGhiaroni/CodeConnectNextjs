81b924f4b777a041da818653eaf07e76a932386f
GGhiaroni <gghiaronitiziano@gmail.com>
2025-06-05 14:11:10 -0300

    utilizando imagens externas

--- src/components/Avatar/index.jsx
+++ src/components/Avatar/index.jsx
@@ -4,7 +4,12 @@ export const Avatar = ({ name, imageSrc }) => {
   return (
     <ul>
       <li>
-        <Image src={imageSrc} />
+        <Image
+          src={imageSrc}
+          width={32}
+          height={32}
+          alt={`Avatar do usuário(a) ${name}`}
+        />
       </li>
       <li>@{name}</li>
     </ul>
