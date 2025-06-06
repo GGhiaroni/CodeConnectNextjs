4093887d1350e9dac711f1736516beda22aede51
GGhiaroni <gghiaronitiziano@gmail.com>
2025-06-06 08:49:49 -0300

    importando fontes do Google Fonts

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,4 +1,5 @@
 import { Aside } from "@/components/Aside";
+import { Prompt } from "next/font/google";
 import "./globals.css";
 
 export const metadata = {
@@ -6,9 +7,15 @@ export const metadata = {
   description: "Por Gabriel Ghiaroni",
 };
 
+const prompt = Prompt({
+  weight: ["400", "600"],
+  subsets: ["latin"],
+  display: "swap",
+});
+
 export default function RootLayout({ children }) {
   return (
-    <html lang="pt-BR">
+    <html lang="pt-BR" className={prompt.className}>
       <body>
         <div className="app-container">
           <Aside />
