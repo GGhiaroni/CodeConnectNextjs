45e50c90e53ce7597204f1884bc36667effb64d0
GGhiaroni <gghiaronitiziano@gmail.com>
2025-06-07 17:33:42 -0300

    colocando o grid na aplicacao

--- src/components/Aside/index.jsx
+++ src/components/Aside/index.jsx
@@ -3,8 +3,8 @@ import logo from "./logo.png";
 
 export const Aside = () => {
   return (
-    <aside className="rounded-lg px-4 py-10 bg-[#171D1F]">
-      <Image src={logo} alt="Logo Code Connect" />
+    <aside className="rounded-lg px-4 py-10 bg-[#171D1F] h-full">
+      <Image src={logo} alt="Logo Code Connect" width={700} height={700} />
     </aside>
   );
 };
