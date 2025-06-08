697c3b42bf2afaa9b35923b35bcff13984d22c9d
GGhiaroni <gghiaronitiziano@gmail.com>
2025-06-08 12:26:21 -0300

    finalizando o projeto

--- src/app/posts/[slug]/page.js
+++ src/app/posts/[slug]/page.js
@@ -1,4 +1,7 @@
+import { CardPost } from "@/components/CardPost";
 import logger from "@/logger";
+import { remark } from "remark";
+import html from "remark-html";
 
 async function getPostBySlug(slug) {
   const url = `http://localhost:3042/posts?slug=${slug}`;
@@ -12,12 +15,28 @@ async function getPostBySlug(slug) {
   if (data.length == 0) {
     return {};
   }
-  return data[0];
+
+  const post = data[0];
+
+  const processedContent = await remark().use(html).process(post.markdown);
+  const contentHtml = processedContent.toString();
+
+  post.markdown = contentHtml;
+
+  return post;
 }
 
 const PagePost = async ({ params }) => {
   const post = await getPostBySlug(params.slug);
-  return <h1 className="text-amber-50">{`título do post: ${post.title}`}</h1>;
+  return (
+    <div>
+      <CardPost post={post} highlight />
+      <h3 className="subtitle">Código:</h3>
+      <div className="code">
+        <div dangerouslySetInnerHTML={{ __html: post.markdown }} />
+      </div>
+    </div>
+  );
 };
 
 export default PagePost;
